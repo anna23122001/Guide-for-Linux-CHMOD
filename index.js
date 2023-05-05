53ee33cbc3eb70b8826cba070226df986bc8ee55
anna <anna.riazantseva123321@gmail.com>
2023-05-05 18:00:17 +0300

    switch/break/default

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+let a = prompt("Enter the number from 0 to 7");
+
+switch (a) {
+case '0':
+    console.log("Level0");
+    break;
+case '1':
+    console.log("Level1");
+    break;
+case '2':
+    console.log('Level2');
+    break;
+case '3':
+    console.log('Level3');
+    break;
+case '4':
+    console.log('Level4');
+    break;
+case '5':
+    console.log('Level5');
+    break;
+case '6':
+    console.log('Level6');
+    break;
+case '7':
+    console.log('Level7');
+    break;
+
+  default:
+    console.log('Error');
+}
